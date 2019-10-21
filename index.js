4d208bc8ba0bf16f9a8422a2fac68008629ac9a3
“Mark <mkanko66@gmail.com>
2019-10-21 15:23:48 -0700

    lab completed

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function() {
+    return 2000 / 100 
+}
+
+// const square = (sum) => {
+//     return sum * sum 
+// }
+
+const square = sum => sum * sum 
+
+const add = (num1, num2) => num1 + num2 
